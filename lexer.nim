f5721fbfcbc558621e67cf8d876af29eface8e9e
Anders B. Clausen <anbclausen@gmail.com>
2021-03-11 00:44:50 +0100

    Fixed indentation in lexer to 4 spaces pr tab.

--- lexer.nim
+++ lexer.nim
@@ -1,48 +1,48 @@
 type
-  TokenType* = enum
-    LAMBDA,
-    DOT,
-    LPAREN,
-    RPAREN,
-    ID,
-  Token* = ref object
-    case ttype* : TokenType
-    of ID:
-      name*: string
-    else:
-      discard
+    TokenType* = enum
+        LAMBDA,
+        DOT,
+        LPAREN,
+        RPAREN,
+        ID,
+    Token* = ref object
+        case ttype* : TokenType
+        of ID:
+            name*: string
+        else:
+            discard
 
 func baseToken(c: char): bool =
-  c == '\\' or c == '.' or c == '(' or c == ')' or c == ' '
+    c == '\\' or c == '.' or c == '(' or c == ')' or c == ' '
 
 func readWord(source: string, i: int): (string, int) =
-  ## Reads word from `source` string from given index `i` until a base token is found.
-  ## Returns the word and length of that word.
+    ## Reads word from `source` string from given index `i` until a base token is found.
+    ## Returns the word and length of that word.
   
-  var str = ""
-  var j = 0
-  while i + j < source.len and not source[i + j].baseToken():
-    str.add(source[i + j])
-    j += 1
-  (str, j - 1)
+    var str = ""
+    var j = 0
+    while i + j < source.len and not source[i + j].baseToken():
+        str.add(source[i + j])
+        j += 1
+    (str, j - 1)
 
 func tokenize*(source: string): seq[Token] = 
-  var i = 0
-  while i < source.len:
-    case source[i]:
-      of '\\':
-        result.add(Token(ttype: LAMBDA))
-      of '.':
-        result.add(Token(ttype: DOT))
-      of '(':
-        result.add(Token(ttype: LPAREN))
-      of ')':
-        result.add(Token(ttype: RPAREN))
-      of ' ':      # just skip white space
-         i += 1
-         continue
-      else:        # must be ID term
-        let (name, len) = readWord(source, i)
-        i += len
-        result.add(Token(ttype: ID, name: name))
-    i += 1
+    var i = 0
+    while i < source.len:
+        case source[i]:
+        of '\\':
+            result.add(Token(ttype: LAMBDA))
+        of '.':
+            result.add(Token(ttype: DOT))
+        of '(':
+            result.add(Token(ttype: LPAREN))
+        of ')':
+            result.add(Token(ttype: RPAREN))
+        of ' ':      # just skip white space
+            i += 1
+            continue
+        else:        # must be ID term
+            let (name, len) = readWord(source, i)
+            i += len
+            result.add(Token(ttype: ID, name: name))
+        i += 1
