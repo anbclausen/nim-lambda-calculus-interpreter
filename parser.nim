55a2755cc202b9792552a313a9780d9993e4053a
Anders B. Clausen <anbclausen@gmail.com>
2021-03-13 21:37:26 +0100

    Removed atom inner function

--- parser.nim
+++ parser.nim
@@ -19,15 +19,6 @@ proc matchingParen(prog: seq[Token]): (seq[Token], seq[Token]) =
         (prog[0..<i-1], prog[i..<prog.len])
 
 proc parse*(prog: seq[Token]): Term =
-    func atom(prog: seq[Token]): Term =
-        case prog:
-            of [Token(ttype: ID, name: @name)]:
-                return Term(kind: Var, id: name)
-            of [Token(ttype: LPAREN), all @tail]:
-                if tail[^1].ttype == RPAREN:
-                    return parse(tail[0..^2])
-                else:
-                    raise newException(Exception, "λ-Parse Error: Error while parsing atom.") # Should be unreachable
     func app(prog: seq[Token]): Term =
         func findAtoms(subprog: seq[Token]): seq[seq[Token]] =
             case subprog:
@@ -39,20 +30,22 @@ proc parse*(prog: seq[Token]): Term =
                     return @[@[Token(ttype: ID, name: name)]] & findAtoms(subprog[1..^1])
                 of [Token(ttype: LPAREN), all @tail]:
                     let (inner, rest) = matchingParen(tail)
-                    return @[@[Token(ttype: LPAREN)] & inner & @[Token(ttype: RPAREN)]] & findAtoms(rest)
+                    return @[inner] & findAtoms(rest)
                 else:
                     raise newException(Exception, "λ-Parse Error: Illegal AST.")
         func genTerm(atoms: seq[seq[Token]]): Term =
             case atoms.len:
                 of 1:
-                    return atom(atoms[0])
+                    return parse(atoms[0])
                 else: 
-                    return Term(kind: App, t1: genTerm(atoms[0..^2]), t2: atom(atoms[^1]))
+                    return Term(kind: App, t1: genTerm(atoms[0..^2]), t2: parse(atoms[^1]))
         return genTerm(findAtoms(prog))
     func term(prog: seq[Token]): Term =
         case prog:
             of [Token(ttype: LAMBDA), Token(ttype: ID, name: @name), Token(ttype: DOT), all @body]:
                 return Term(kind: Abs, param: name, body: parse(body))
+            of [Token(ttype: ID, name: @name)]:
+                return Term(kind: Var, id: name)
             else:
                 return app(prog)
     return term(prog)
