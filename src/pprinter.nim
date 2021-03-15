764709c88ae3daa5a4db35796882055643612abe
Anders B. Clausen <anbclausen@gmail.com>
2021-03-15 20:52:26 +0100

    Now can pprint new tokens and terms

--- src/pprinter.nim
+++ src/pprinter.nim
@@ -2,23 +2,29 @@ import ast, lexer, strformat
 
 func pprint*(t: T): string =
     case t.t:
-    of Var:
-        t.id
-    of Abs:
-        fmt"(λ{t.param}.{pprint(t.body)})"
-    of App:
-        fmt"({pprint(t.t1)} {pprint(t.t2)})"
+        of Var:
+            t.id
+        of Abs:
+            fmt"(λ{t.param}.{pprint(t.body)})"
+        of App:
+            fmt"({pprint(t.t1)} {pprint(t.t2)})"
+        of Def:
+            fmt"{t.name} := {pprint(t.val)}"
+        of Empty:
+            ""
 
 func pprint*(tokens: seq[Token]): string =    # to pretty print the lexer's output
     for i in 0 ..< tokens.len:
         case tokens[i].ttype:
-        of LAMBDA:
-            result.add("LAMBDA ")
-        of DOT:
-            result.add("DOT ")
-        of RPAREN:
-            result.add("RPAREN ")
-        of LPAREN:
-            result.add("LPAREN ")
-        of ID:
-            result.add(tokens[i].name & " ")
+            of LAMBDA:
+                result.add("LAMBDA ")
+            of DOT:
+                result.add("DOT ")
+            of RPAREN:
+                result.add("RPAREN ")
+            of LPAREN:
+                result.add("LPAREN ")
+            of ID:
+                result.add(tokens[i].name & " ")
+            of DEFAS:
+                result.add("DEFAS ")
