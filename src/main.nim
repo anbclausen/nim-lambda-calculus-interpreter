ad389ae96a023becc6798aff6ff91cd6fd8a4d6b
Anders B. Clausen <anbclausen@gmail.com>
2021-03-16 23:54:06 +0100

    It actually works in JS, wtf

--- src/main.nim
+++ src/main.nim
@@ -1,15 +1,8 @@
-import strutils, parser, lexer, pprinter, interpreter, sequtils
+import parser, lexer, pprinter, interpreter, sequtils
 
-while true:
-    write(stdout, "λ] ")
-    let inp = readLine(stdin).strip()
-
-    if inp == "quit":
-        echo "Goodbλe!"
-        break
+proc run(inp: cstring): cstring {.exportc.} =
     let tokens = tokenize(toSeq(inp.items))
     let ast = parse(tokens)
     let res = eval(ast)
     let print = pprint(res)
-    if print != "":
-        echo print
+    cstring(print)
